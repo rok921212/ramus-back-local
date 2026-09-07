8830440db3c791e0005c76cc1ec6e99d448ef783
unknown <smitthapa808@gmail.com>
2026-09-07 23:23:02 +0545

    new

--- config/env.config.js
+++ config/env.config.js
@@ -4,9 +4,9 @@ module.exports = {
   "NODE_ENV": "production",
   "ADMIN_CODE": "9804344434",
   "SESSION_SECRET": "RenderHostedScoreSync_2024_SuperSecureKey_ReplaceInProd123!",
-  "MONGODB_URI": "mongodb+srv://demon:P6whwJ8qsMfIZg2F@cluster0.ix4q7ng.mongodb.net/?appName=Cluster0",
-  "UPSTASH_REDIS_REST_URL": "https://dear-polecat-206495.upstash.io",
-  "UPSTASH_REDIS_REST_TOKEN": "gQAAAAAAAyafAAIgcDEzZGJlOTAyODMwODk0NGE3OGZjZmI3OTIwMzYzYTA2OA",
+  "MONGODB_URI": "mongodb+srv://r45207415_db_user:Sz4UZUZrdfdJ6fj6@cluster0.5bwkapg.mongodb.net/?appName=Cluster0",
+  "UPSTASH_REDIS_REST_URL": "https://amazed-blowfish-81101.upstash.io",
+  "UPSTASH_REDIS_REST_TOKEN": "gQAAAAAAATzNAAIgcDI4MDU0ZTg2NWQwZTM0ZTdhOTRlOGNhY2RhZmYxYjVlMA",
   "LOG_LEVEL": "info",
   "LOG_TO_FILE": "true"
 };
